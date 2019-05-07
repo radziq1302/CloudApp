85bf4f7dc451334958b4cad7f2b75d0eb100f403
radziq1302 <radziq1302@gmail.com>
2019-05-08 00:31:50 +0200

    comit 2'

--- app/src/main/java/com/example/cloudapp/MainActivity.java
+++ app/src/main/java/com/example/cloudapp/MainActivity.java
@@ -2,20 +2,46 @@ package com.example.cloudapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.amazonaws.amplify.generated.graphql.ListPetsQuery;
 import com.amazonaws.mobile.client.AWSMobileClient;
+import com.amazonaws.mobileconnectors.appsync.fetcher.AppSyncResponseFetchers;
+import com.apollographql.apollo.GraphQLCall;
+import com.apollographql.apollo.api.Response;
+import com.apollographql.apollo.exception.ApolloException;
+
+import java.util.ArrayList;
+
+import javax.annotation.Nonnull;
 
 public class MainActivity extends AppCompatActivity {
     private Button wyloguj;
-
+    RecyclerView mRecyclerView;
+    MyAdapter mAdapter;
+    private ArrayList<ListPetsQuery.Item> mPets;
+    private final String TAG = MainActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mRecyclerView = findViewById(R.id.recycler_view);
+
+        // use a linear layout manager
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        // specify an adapter (see also next example)
+        mAdapter = new MyAdapter(this);
+        mRecyclerView.setAdapter(mAdapter);
+
+        ClientFactory.init(this);
         wyloguj = (Button) findViewById(R.id.logout);
             wyloguj.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -24,7 +50,53 @@ public class MainActivity extends AppCompatActivity {
                     signOut(v);
                 }
             });
+        FloatingActionButton btnAddPet = findViewById(R.id.btn_addPet);
+        btnAddPet.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent addPetIntent = new Intent(MainActivity.this, AddDataActivity.class);
+                MainActivity.this.startActivity(addPetIntent);
+            }
+        });
     }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        // Query list data when we return to the screen
+        query();
+    }
+
+    public void query(){
+        ClientFactory.appSyncClient().query(ListPetsQuery.builder().build())
+                .responseFetcher(AppSyncResponseFetchers.CACHE_AND_NETWORK)
+                .enqueue(queryCallback);
+    }
+
+    private GraphQLCall.Callback<ListPetsQuery.Data> queryCallback = new GraphQLCall.Callback<ListPetsQuery.Data>() {
+        @Override
+        public void onResponse(@Nonnull Response<ListPetsQuery.Data> response) {
+
+            mPets = new ArrayList<>(response.data().listPets().items());
+
+            Log.i(TAG, "Retrieved list items: " + mPets.toString());
+
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    mAdapter.setItems(mPets);
+                    mAdapter.notifyDataSetChanged();
+                }
+            });
+        }
+
+        @Override
+        public void onFailure(@Nonnull ApolloException e) {
+            Log.e(TAG, e.toString());
+        }
+    };
     public void signOut(View view) {
         AWSMobileClient.getInstance().signOut();
         Intent a = new Intent(MainActivity.this, AuthenticationActivity.class);
